e9f78bea97634f14407cf489002d08275a033532
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-07 10:47:22 -0700

    add e listener for details node list

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 //grab elements from the DOM
 
-const details = document.querySelector(".accordion__details");
-const plusSign =  document.querySelector(".accordion__icon");
+let details = document.querySelectorAll(".accordion__details");
+const plusSigns =  document.querySelectorAll(".accordion__icon");
+
+//test area
+
+//add a way to make each plus sign turn to a minus sign when clicked, then back to plus when clicked again
+
+plusSigns.forEach((plus) => {
+    //let details = document.querySelectorAll(".accordion__details");
+    plus.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("plus sign e listener working");
+    if (plus.textContent === "+") {
+        plus.textContent = "-";
+    } else {
+        plus.textContent = "+"
+        }
+    })
+            
+})
 
 
-for (let i=0; i < details.clientHeight; i++) {
-    plusSign.addEventListener('click', function(e) {
-        details.style.height = "auto";
-        details.style.overflow = "auto";
+    for (let i=0; i < details.length; i++) {
+        plusSigns[i].addEventListener('click', function(e) {
+        console.log("the details e listener worked!");
+        details[i].style.backgroundColor = "pink";
     })
 }
