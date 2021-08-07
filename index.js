617031dcc13c9dcc3f404308266e2448228f7e7e
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-08-07 11:03:32 -0700

    fix logic in the 'details' e listener

--- index.js
+++ index.js
@@ -21,12 +21,19 @@ plusSigns.forEach((plus) => {
             
 })
 
+//make it so the corresponding answer to the question expands down when the plus sign for that question is clicked.
 
     for (let i=0; i < details.length; i++) {
         plusSigns[i].addEventListener('click', function(e) {
+            if (details[i].style.overflow === "hidden" || details[i].style.overflow === "") {
         console.log("the details e listener worked!");
-        //details[i].style.maxHeight = "75vh";
-        //details[i].style.overflow = "visible";
-        details[i].classList.remove = "accordion__shrink";
+        details[i].style.maxHeight = "75vh";
+        details[i].style.overflow = "visible";
+        details[i].style.backgroundColor = "lightGrey";
+            } else {
+            details[i].style.maxHeight = "0";
+            details[i].style.overflow = "hidden";
+            }
+        //details[i].classList.remove = "accordion__shrink"; I couldn't get the toggle method to work within this for loop, why?
     })
 }
